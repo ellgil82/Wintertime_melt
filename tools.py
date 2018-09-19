762c46a31dda10e9d37a4a6cf0a80d2f5e0dd6af
ellgil82 <ellgil82@bas.ac.uk>
2018-09-19 13:48:27 +0100

    Added functions to deal with time
    Convert dates and times in AWS decimal days to python datetime format

--- tools.py
+++ tools.py
@@ -33,3 +33,24 @@ def find_gridbox(x, y, real_lat, real_lon):
     lat_index = np.argmin((real_lat - x) ** 2)  # take whole array and subtract lat you want from
     lon_index = np.argmin((real_lon - y) ** 2)  # each point, then find the smallest difference
     return lon_index, lat_index
+
+## Functions to convert AWS time and dates (in decimal days and years) into datetime objects for plotting
+
+def compose_date(years, months=1, days=1, weeks=None, hours=None, minutes=None,
+                 seconds=None, milliseconds=None, microseconds=None, nanoseconds=None):
+    years = np.asarray(years) - 1970
+    months = np.asarray(months) - 1
+    days = np.asarray(days) - 1
+    # convert to 0-based
+    types = ('<M8[Y]', '<m8[M]', '<m8[D]', '<m8[W]', '<m8[h]',
+             '<m8[m]', '<m8[s]', '<m8[ms]', '<m8[us]', '<m8[ns]')
+    vals = (years, months, days, weeks, hours, minutes, seconds,
+            milliseconds, microseconds, nanoseconds)
+    return sum(np.asarray(v, dtype=t) for t, v in zip(types, vals)
+               if v is not None)
+
+def compose_time(hours=None, minutes=None, seconds=None, milliseconds=None, microseconds=None, nanoseconds=None):
+    types = ('<m8[h]','<m8[m]', '<m8[s]', '<m8[ms]', '<m8[us]', '<m8[ns]')
+    vals = (hours, minutes, seconds, milliseconds, microseconds, nanoseconds)
+    return sum(np.asarray(v, dtype=t) for t, v in zip(types, vals)
+               if v is not None)
